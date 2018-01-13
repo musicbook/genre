035888250b10426adaa7622931cb1e4d385651fd
Jan Blatnik <jb4634@student.uni-lj.si>
2018-01-13 04:46:23 +0100

    Dodal log

--- src/main/java/com/fri/musicbook/GenreApplication.java
+++ src/main/java/com/fri/musicbook/GenreApplication.java
@@ -2,10 +2,12 @@ package com.fri.musicbook;
 
 //import com.kumuluz.ee.discovery.annotations.RegisterService;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
-//@RegisterService
+@RegisterService
 public class GenreApplication extends Application {
 }

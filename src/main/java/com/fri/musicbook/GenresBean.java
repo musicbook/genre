bb26335c82f5f377f19babac30992eac1f850e68
Jan Blatnik <jb4634@student.uni-lj.si>
2018-01-14 15:38:29 +0100

    Popravek discovery: tip basepath spremenjen v String

--- src/main/java/com/fri/musicbook/GenresBean.java
+++ src/main/java/com/fri/musicbook/GenresBean.java
@@ -43,7 +43,7 @@ public class GenresBean {
 
     @Inject
     @DiscoverService("artist-service")
-    private Optional<String> basePath;
+    private String basePath;
 
     //private String basePath = "http://172.17.0.1:8084";
 
